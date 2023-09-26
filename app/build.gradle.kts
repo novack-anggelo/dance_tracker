6a267a45e0e3065af41983a0785b4337e2cc99ea
novack-anggelo <anggelo.novack@gmail.com>
2023-09-26 14:21:57 +0200

    #1: migrating app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,10 @@
-/*plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    kotlin("kapt")
-    id("com.google.dagger.hilt.android")
-}*/
+plugins {
+    id("dance_tracker.android.application")
+    id("dance_tracker.android.application.compose")
+    id("dance_tracker.android.hilt")
+}
 
-/*android {
+android {
     namespace = "com.novack.dance_tracker"
     compileSdk = 34
 
@@ -23,24 +22,15 @@
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = DanceTrackerBuildType.DEBUG.applicationIdSuffix
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
-    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -49,53 +39,4 @@
 }
 
 dependencies {
-
-    val androidxComposeVersion = "1.7.2"
-    val androidxCoreVersion = "1.12.0"
-    val androidxDatastore = "1.0.0"
-    val coroutinesVersion = "1.6.4"
-    val datastoreVersion = "1.0.0"
-    val hiltVersion = "2.44"
-    val junitVersion = "4.13.2"
-    val lifecycleRuntimeVersion = "2.6.2"
-    val nav_version = "2.7.3"
-    val roomVersion = "2.5.2"
-
-
-
-    implementation("androidx.core:core-ktx:$androidxCoreVersion")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleRuntimeVersion")
-    implementation("androidx.activity:activity-compose:$androidxComposeVersion")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:$junitVersion")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    implementation("androidx.room:room-runtime:$roomVersion")
-    annotationProcessor("androidx.room:room-compiler:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
-
-    implementation("com.google.dagger:hilt-android:$hiltVersion")
-    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
-
-    implementation("androidx.datastore:datastore:$androidxDatastore")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
-
-    implementation("androidx.datastore:datastore-core:$datastoreVersion")
-    implementation("androidx.datastore:datastore-preferences:$datastoreVersion")
-
-    implementation("androidx.navigation:navigation-compose:$nav_version")
 }
-
-kapt {
-    correctErrorTypes = true
-}*/
--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -25,3 +25,44 @@ dependencies {
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
 }
+
+gradlePlugin {
+    plugins {
+        register("androidApplicationCompose") {
+            id = "dance_tracker.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("androidApplication") {
+            id = "dance_tracker.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("androidLibraryCompose") {
+            id = "dance_tracker.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+        register("androidLibrary") {
+            id = "dance_tracker.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        /*register("androidFeature") {
+            id = "dance_tracker.android.feature"
+            implementationClass = "AndroidFeatureConventionPlugin"
+        }*/
+        register("androidTest") {
+            id = "dance_tracker.android.test"
+            implementationClass = "AndroidTestConventionPlugin"
+        }
+        register("androidHilt") {
+            id = "dance_tracker.android.hilt"
+            implementationClass = "AndroidHiltConventionPlugin"
+        }
+        register("androidRoom") {
+            id = "dance_tracker.android.room"
+            implementationClass = "AndroidRoomConventionPlugin"
+        }
+        register("jvmLibrary") {
+            id = "dance_tracker.jvm.library"
+            implementationClass = "JvmLibraryConventionPlugin"
+        }
+    }
+}
